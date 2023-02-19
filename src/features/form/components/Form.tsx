fde497aa4c3179d6bc6069b83c8b98e4f36f2b37
Kosuke-Kg <kosuke.kg1127@gmail.com>
2023-02-19 14:35:44 +0900

    add remove 追加

--- src/features/form/components/Form.tsx
+++ src/features/form/components/Form.tsx
@@ -53,18 +53,39 @@ const Forms: FC = () => {
       </Stack>
       {
         fields.map((f, i) => (
-          <Box key={f.id} >
-            <Typography>商品名</Typography>
-            <TextField
-              {...form.register(`field.${i}.itemName`)}
-            />
-            <Typography>価格</Typography>
-            <TextField
-              {...form.register(`field.${i}.price`)}
-            />
+          <Box
+            key={f.id}
+            border={1}
+            p={2}
+          >
+            <Stack
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="center"
+              spacing={4}
+            >
+              <Typography>商品名</Typography>
+              <TextField
+                {...form.register(`field.${i}.itemName`)}
+              />
+            </Stack>
+            <Stack
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="center"
+              spacing={4}
+            >
+              <Typography>価格</Typography>
+              <TextField
+                {...form.register(`field.${i}.price`)}
+              />
+            </Stack>
+
+            <Button onClick={()=>fieldRemove(i)}>削除</Button>
           </Box>
         ))
       }
+      <Button onClick={() => fieldAdd()}>追加</Button>
       <Stack />
 
       <Button type="submit" >コンソールに表示</Button>
